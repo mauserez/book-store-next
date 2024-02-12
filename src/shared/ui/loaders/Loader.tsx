22bc36bdb774a040d09e629d94295ebdcf52ed23
HomeMac <mauserez@mail.ru>
2024-02-12 22:54:16 +0300

    no check

--- src/shared/ui/loaders/Loader.tsx
+++ src/shared/ui/loaders/Loader.tsx
@@ -1,5 +1,5 @@
-import { InfinitySpin } from "react-loader-spinner";
 // @ts-nocheck
+import { InfinitySpin } from "react-loader-spinner";
 import s from "./Loader.module.css";
 
 export default function Loader() {
