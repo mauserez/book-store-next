c58577c9cde9e06cfae20aa63ec0d51b74eff27e
HomeMac <mauserez@mail.ru>
2024-02-12 22:47:12 +0300

    fix loader deploy bug

--- src/shared/ui/loaders/Loader.tsx
+++ src/shared/ui/loaders/Loader.tsx
@@ -2,13 +2,16 @@ import { InfinitySpin } from "react-loader-spinner";
 import s from "./Loader.module.css";
 
 export default function Loader() {
+	const props = {
+		visible: true,
+		width: 200,
+		color: "#756ad3",
+		ariaLabel: "infinity-spin-loading",
+	};
+
 	return (
 		<div className={s.loaderWrap}>
-			<InfinitySpin
-				width="200"
-				color="#756ad3"
-				ariaLabel="infinity-spin-loading"
-			/>
+			<InfinitySpin {...props} />
 		</div>
 	);
 }
